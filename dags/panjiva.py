42142ba844567dd581dc7596be63bedf5205bd7d
Vitaly <VitaliyPetrov18@gmail.com>
2020-03-06 14:12:02 +0300

    all parts except hive queries works fine

--- dags/panjiva.py
+++ dags/panjiva.py
@@ -1,6 +1,5 @@
-import os
 import airflow
-from datetime import datetime, timedelta
+from datetime import datetime
 
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
@@ -11,7 +10,7 @@ from airflow.contrib.hooks.ssh_hook import SSHHook
 
 args = {
     'owner': 'vit',
-    # when set to True, keeps a task from getting triggered if the previous schedule for the task hasn’t succeeded
+    # when set to True, keeps a task from getting triggered if the previous schedule for the task failed
     'depends_on_past': True,
     # Dict of global variables to be used within DAG
     'provide_context': True,
@@ -26,7 +25,7 @@ def dummy_python_operator(ds, **kwards):
 
 
 with airflow.DAG(
-        dag_id='panjiva-processing',
+        dag_id='panjiva',
         schedule_interval="@once",
         default_args=args
 ) as dag:
@@ -75,22 +74,47 @@ with airflow.DAG(
         timeout=30
     )
     ############################################################
-
+    '''
+    # Use bash scripts if there is an interconnect with Hadoop/HDFS
     download_import_data = BashOperator(
         task_id='download-panjiva-import-data',
         bash_command='${AIRFLOW_HOME}/dags/downloader-scripts/download-import-data.sh ',
         dag=dag
     )
-
+    
     download_export_data = BashOperator(
         task_id='download-panjiva-export-data',
         bash_command='${AIRFLOW_HOME}/dags/downloader-scripts/download-export-data.sh ',
         dag=dag
     )
-
+    
     parse_and_put = BashOperator(
         task_id='parse-panjiva-archievies-and-put-2hdfs',
         bash_command='${AIRFLOW_HOME}/dags/parsing-scripts/parse-panjiva-and-put.sh ',
+        dag=dag
+    )
+    #
+    '''
+
+    download_import_data = SSHOperator(
+        task_id='download-panjiva-import-data',
+        remote_host='10.1.25.37',
+        ssh_hook=hadoop_hook,
+        command=u'/data/demo/download-import-data.sh '
+    )
+
+    download_export_data = SSHOperator(
+        task_id='download-panjiva-export-data',
+        remote_host='10.1.25.37',
+        ssh_hook=hadoop_hook,
+        command=u'/data/demo/download-export-data.sh '
+    )
+
+    parse_and_put = SSHOperator(
+        task_id='parse-panjiva-archievies-and-put-2hdfs',
+        remote_host='10.1.25.37',
+        ssh_hook=hadoop_hook,
+        command=u'/data/demo/parse-panjiva-and-put.sh ',
         dag=dag
     )
 
@@ -109,7 +133,7 @@ with airflow.DAG(
         task_id='upload-import-data-to-hive',
         remote_host='10.1.25.37',
         ssh_hook=hadoop_hook,
-        command=u'echo $HOSTNAME',
+        command=u'/data/demo/submit-df-handler.sh ',
         dag=dag
     )
 
@@ -118,7 +142,8 @@ with airflow.DAG(
 
     companies_info = HiveOperator(
         task_id='united-companies-table',
-        hql='${AIRFLOW_HOME}/sql/companies_info.sql ',
+        #hql='${AIRFLOW_HOME}/sql/companies_info.sql ',
+        hql='select * from companies_info',
         hive_cli_conn_id='hive'
     )
 
